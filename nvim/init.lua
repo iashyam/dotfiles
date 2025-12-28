ef8dc817b05d426ff451f17bc704656911125067
shyam sunder <shyam10kwd@gmail.com>
2025-12-28 15:29:35 +0530

    new neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,54 +1,167 @@
-vim.cmd('set expandtab')
-vim.cmd('set tabstop=4')
-vim.cmd('set shiftwidth=4')
-vim.cmd('set rnu')
-vim.cmd('set number')
-vim.g.mapleader=' '
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
 
--- the lazy plugin manager
+-- leader
+vim.g.mapleader = " "
+
+
+-- lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
-local opts = {}
-local plugins = {
- { "catppuccin/nvim", name = "catppuccin", priority = 1000 }, 
- {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+require("lazy").setup({
+  -- LSP
+  "neovim/nvim-lspconfig",
+
+  -- breakcet 
+  "windwp/nvim-autopairs",
+
+  -- completion
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+
+  -- telescope
+  {
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
   },
 
-  {"nvim-treesitter/nvim-treesitter", build= ":TSUpdate"}, 
-  {'github/copilot.vim'}
+ {
+  "ray-x/lsp_signature.nvim",
+  event = "LspAttach",
+  opts = {
+    bind = true,
+    floating_window = true,
+    hint_enable = false, -- VS Code–style popup only
+  },
+ },
 
-}
+ {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  opts = {},
+ },
 
-require("lazy").setup(plugins, opts)
+ {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = { "python", "go", "lua" },
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
+ },
 
---require catppuccin
-require("catppuccin").setup()
+ --harpoon
+ {
+  "ThePrimeagen/harpoon",
+  dependencies = { "nvim-lua/plenary.nvim" },
+ },
 
---set the colorscheme to it!
-vim.cmd.colorscheme "catppuccin"
+
+  -- colors
+  "tanvirtin/monokai.nvim",
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+ })
+
+ 
+
+-- LSP
+
+vim.lsp.config("pyright", {})
+vim.lsp.config("goplus", {})
+
+-- LSP (Neovim 0.11+ correct way)
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+vim.lsp.enable("pyright")
+vim.lsp.enable("gopls")
+
+vim.lsp.config("pyright", {
+  capabilities = capabilities,
+})
+
+vim.lsp.config("gopls", {
+  capabilities = capabilities,
+})
+
+
+-- completion (VS Code–like)
+
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+local cmp = require("cmp")
+
+cmp.setup({
+  completion = {
+    autocomplete = { cmp.TriggerEvent.TextChanged },
+  },
+  sources = {
+    { name = "nvim_lsp" }, -- relevance comes from here
+  },
+})
+
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+vim.lsp.config("pyright", {
+  capabilities = capabilities,
+})
+
+vim.lsp.config("gopls", {
+  capabilities = capabilities,
+})
+
+--colorscheme
+
+vim.cmd("colorscheme catppuccin")
+--vim.cmd("colorscheme monokai")
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none" })
 
 -- telescope setup
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
+require("telescope").setup({})
+vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<cr>")
+vim.keymap.set("n", "<leader>g", "<cmd>Telescope live_grep<cr>")
+vim.keymap.set("n", "<leader>fa", "<cmd>Telescope find_files cwd=/<cr>")
+vim.keymap.set("n", "<leader>fA", "<cmd>Telescope live_grep cwd=/<cr>")
 
--- treesitter config
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"python", "lua", "javascript", "go"},
-  highlight = { enable = true },
-  indent = { enable = true }
-})
+vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" })
+
+-- harpoon settings
+-- Harpoon (stable v1)
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
+
